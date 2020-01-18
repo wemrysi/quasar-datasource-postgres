51b5d88ed0acf6064b7fe658088a8f1a58ca117f
Emrys Ingersoll <ingersoll@gmail.com>
2020-01-17 18:02:46 -0600

    ByteStore support

--- core/src/test/scala/quasar/plugin/postgres/datasource/PostgresDatasourceModuleSpec.scala
+++ core/src/test/scala/quasar/plugin/postgres/datasource/PostgresDatasourceModuleSpec.scala
@@ -26,7 +26,7 @@ import cats.implicits._
 import quasar.{EffectfulQSpec, RateLimiter, NoopRateLimitUpdater, RateLimiting}
 import quasar.api.datasource.DatasourceError
 import quasar.api.resource.ResourcePath
-import quasar.connector.ResourceError
+import quasar.connector.{ByteStore, ResourceError}
 import quasar.contrib.scalaz.MonadError_
 
 import java.util.UUID
@@ -53,7 +53,7 @@ object PostgresDatasourceModuleSpec extends EffectfulQSpec[IO] {
       val cfg = Json("malformed" := true)
 
       rateLimiting.flatMap(rl =>
-        PostgresDatasourceModule.lightweightDatasource[IO, UUID](cfg, rl) use {
+        PostgresDatasourceModule.lightweightDatasource[IO, UUID](cfg, rl, ByteStore.void[IO]) use {
           case Left(DatasourceError.MalformedConfiguration(_, c, _)) =>
             IO.pure(c must_=== jString(Redacted))
 
@@ -65,7 +65,7 @@ object PostgresDatasourceModuleSpec extends EffectfulQSpec[IO] {
       val cfg = Json("connectionUri" := "postgresql://localhost:1234/foobar?user=alice&password=secret")
 
       rateLimiting.flatMap(rl =>
-        PostgresDatasourceModule.lightweightDatasource[IO, UUID](cfg, rl) use {
+        PostgresDatasourceModule.lightweightDatasource[IO, UUID](cfg, rl, ByteStore.void[IO]) use {
           case Left(DatasourceError.ConnectionFailed(_, c, _)) =>
             IO.pure(c.some must_=== cfg.as[Config].map(_.sanitized.asJson).toOption)
 
@@ -77,7 +77,7 @@ object PostgresDatasourceModuleSpec extends EffectfulQSpec[IO] {
       val cfg = Json("connectionUri" := "postgresql://localhost:54322/postgres?user=postgres&password=postgres")
 
       rateLimiting.flatMap(rl =>
-        PostgresDatasourceModule.lightweightDatasource[IO, UUID](cfg, rl) use {
+        PostgresDatasourceModule.lightweightDatasource[IO, UUID](cfg, rl, ByteStore.void[IO]) use {
           case Right(ds) =>
             ds.prefixedChildPaths(ResourcePath.root())
               .flatMap(_.sequence.unNone.compile.toList)
